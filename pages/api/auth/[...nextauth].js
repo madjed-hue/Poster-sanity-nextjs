09594ad7972f1c7acc2ad745561de2678efda211
FATEH BEDDIAF <62912697+madjed-hue@users.noreply.github.com>
2023-03-25 15:29:04 +0100

    next auth error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
+
 export default NextAuth(authOptions);
